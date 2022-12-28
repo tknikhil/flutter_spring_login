13e86b460be8a79691344b6c78602b6d036295c5
1114nikhil <nikhiltk1114@gmail.com>
2022-12-28 22:21:21 +0530

    addded textformfield

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,52 +11,88 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        child:  Form(child: Container(
+        child: Form(
+            child: Container(
           height: 500,
           width: MediaQuery.of(context).size.width,
           decoration: const BoxDecoration(
-            color: Color.fromRGBO(129, 3, 54, 1),
-            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40.0),bottomRight: Radius.circular(40.0))
-          ),
+              color: Color.fromRGBO(129, 3, 54, 1),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0))),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                  const SizedBox(height:60),
-                Text("Login",style:GoogleFonts.pacifico(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  color: Colors.white
-                )),
+                const SizedBox(height: 60),
+                Text("Login",
+                    style: GoogleFonts.pacifico(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.white)),
                 const SizedBox(height: 40),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text("Email",style: GoogleFonts.roboto(
-                    // fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: const Color.fromRGBO(255, 255, 255, 0.8),
-
-                  ),),
-                ),
+                // Align(
+                //   alignment: Alignment.topLeft,
+                //   child: Text("Email",style: GoogleFonts.roboto(
+                //     // fontWeight: FontWeight.bold,
+                //     fontSize: 30,
+                //     color: const Color.fromRGBO(255, 255, 255, 0.8),
+                //
+                //   ),),
+                // ),
                 TextFormField(
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Color.fromRGBO(199, 44, 65, .4),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(9))
-
-                    )
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "please enter your email!";
+                    }
+                  },
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Color.fromRGBO(255, 255, 255, 0.8),
                   ),
+                  decoration: const InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontFamily: 'GoogleFonts.roboto'),
+                      filled: true,
+                      fillColor: Color.fromRGBO(199, 44, 65, .4),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(9)))),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "please enter your password!";
+                    }
+                  },
+                  obscureText: true,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Color.fromRGBO(255, 255, 255, 0.8),
+                  ),
+                  decoration: const InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontFamily: 'GoogleFonts.roboto'),
+                      filled: true,
+                      fillColor: Color.fromRGBO(199, 44, 65, .4),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(9)))),
                 )
               ],
             ),
           ),
         )),
       ),
-
     );
   }
 }
